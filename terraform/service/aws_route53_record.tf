85d511714b00969d57bf3fc8bf817d69b5453ea6
Error4577 <donttreadonme4577@gmail.com>
2020-01-11 19:14:38 +0900

    Update aws_route53_record.tf

--- terraform/service/aws_route53_record.tf
+++ terraform/service/aws_route53_record.tf
@@ -9,3 +9,13 @@ resource "aws_route53_record" "error4577" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "error4577_certificate" {
+  zone_id = data.aws_route53_zone.error4577.id
+  name    = aws_acm_certificate.error4577.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.error4577.domain_validation_options[0].resource_record_type
+  records = [
+    aws_acm_certificate.error4577.domain_validation_options[0].resource_record_value
+  ]
+  ttl = 60
+}
