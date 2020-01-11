7c335a57f795e5f66e2161bda6183ef002c72792
Error4577 <donttreadonme4577@gmail.com>
2020-01-11 13:28:11 +0900

    Update template_file.tf

--- terraform/service/template_file.tf
+++ terraform/service/template_file.tf
@@ -5,3 +5,7 @@ data "template_file" "user_data" {
 data "template_file" "service_container_definition" {
   template = file("./container_definitions/service.json.tpl")
 }
+
+data "template_file" "migrate_container_definition" {
+  template = file("./container_definitions/migrate.json.tpl")
+}
