404d7ac5aa2696392988c4b3e91284c726300590
Error4577 <donttreadonme4577@gmail.com>
2020-01-11 19:12:30 +0900

    Create aws_acm_certificate.tf

---
+++ terraform/service/aws_acm_certificate.tf
@@ -0,0 +1,5 @@
+resource "aws_acm_certificate" "error4577" {
+  domain_name               = data.aws_route53_zone.error4577.name
+  subject_alternative_names = []
+  validation_method         = "DNS"
+}
