42adeae180311378eb809a0826ceaa3cb543f799
Error4577 <donttreadonme4577@gmail.com>
2020-01-15 20:49:48 +0900

    Update engineer.rb

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -3,4 +3,5 @@ class Engineer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum sex: { 男性: 0, 女性: 1, その他: 2, 回答しない: nil, }
 end
