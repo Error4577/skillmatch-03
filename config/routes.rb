040e287ac35aa74959c112418c671cdfcce38c4f
Error4577 <donttreadonme4577@gmail.com>
2020-01-15 20:49:16 +0900

    Update routes.rb
    companyとengineerのURLが被るので修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :companies
-  devise_for :engineers
+  devise_for :companies, controllers: {
+    sessions:       'companies/sessions',
+    passwords:      'companies/passwords',
+    registrations:  'companies/registrations'
+  }
+  devise_for :engineers, controllers: {
+    sessions:       'engineers/sessions',
+    passwords:      'engineers/passwords',
+    registrations:  'engineers/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'top#index'
   resources :todos
