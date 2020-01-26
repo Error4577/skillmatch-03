8924971e831b7d97e8de563b710a8d2f8d73b065
Error4577 <donttreadonme4577@gmail.com>
2020-01-26 17:39:38 +0900

    dev company account edit function

--- lib/devise/company_parameter_sanitizer.rb
+++ lib/devise/company_parameter_sanitizer.rb
@@ -1,7 +1,6 @@
 class Company::ParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
-    permit(:sign_up, keys: %i[name email password phone_number about])
-    permit(:account_update, keys: %i[name email phone_number about])
+    permit(:sign_up, keys: %i[name phone_number about])
   end
 end
