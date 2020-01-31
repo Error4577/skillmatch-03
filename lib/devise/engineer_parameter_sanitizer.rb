4e38ea99199ad60a3e9f0e5713cffb3c2f7597a6
Error4577 <donttreadonme4577@gmail.com>
2020-01-31 15:46:41 +0900

    temp

--- lib/devise/engineer_parameter_sanitizer.rb
+++ lib/devise/engineer_parameter_sanitizer.rb
@@ -1,6 +1,6 @@
 class Engineer::ParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
-    permit(:sign_up, keys: %i[name email password sex birthday phone_number self_introduction])
+    permit(:sign_up, keys: %i[name sex birthday phone_number self_introduction])
   end
 end
